5ae42e0010521a1b567f06d92af56a7d4982cda0
TOGURO Mikito <mit@shalab.net>
2016-04-25 03:47:27 +0900

    fork_and_exec をテスト的に実装したが、まだ、仕様が詰め切れていないので、次のコミットまでに仕様を決める
    fork_and_exec の仕様についてまだ以下のことが煮詰まっていない。
    TODO:
    * 関数の戻り値についての仕様をもうはっきりさせる必要がある。
    * 関数自体が長すぎる
    * path が不適当なときにどうするかのチェック
    * argv のチェック？ できる？
    * 作成中に、戻り値が、int だったり pid_t だったりして、安定していない。
     多分 fork の戻り値 pid_t をベースに考えるのが良いと思う。
    * SIGCHLD の動作を考慮していない。 sigaction の動作を加えて考えること。
    * 既に開いている ファイルディスクリプタの操作がまだ。
    * 戻り値の pid_t の意味と、errno の状態についてまとめておくこと。

--- execpath.c
+++ execpath.c
@@ -1,11 +1,15 @@
 ﻿/*
   exec に渡す realpath の処理をテストする。
 
+  TODO：
+  fork_and_exec したときの SIGCHLD の取扱を追加するべき。
  */
 #if defined(HAVE_CONFIG_H)
 #include "config.h"
 #endif /* defined(HAVE_CONFIG_H) */
 
+#include <sys/types.h>
+#include <sys/wait.h>
 #include <unistd.h>
 #include <fcntl.h>
 #include <stdlib.h>
@@ -20,6 +24,12 @@
 
 /* realpath(3) require */
 /* _BSD_SOURCE || _XOPEN_SOURCE >= 500 || _XOPEN_SOURCE && _XOPEN_SOURCE_EXTENDED */
+
+/* パイプの読み出し側 書き込み側のシンボル */
+enum{
+  READ_SIDE = 0,
+  WRITE_SIDE = 1,
+};
 
 int pathconf_path_max( size_t* length );
 char* get_absolute_path( const char* path );
@@ -41,6 +51,7 @@ int pathconf_path_max( size_t* length )
 #endif /* defined( PATH_MAX ) */
 }
 
+// なんか名前がいけてないな。
 char* get_absolute_path( const char* path )
 {
   int err = 0; // realpath に備えて、errno を用意しておく
@@ -100,10 +111,159 @@ int fcntl_set_close_exec( int fd )
   return fcntl_result;
 }
 
-enum{
-  READ_SIDE = 0,
-  WRITE_SIDE = 1,
-};
+
+/**
+   fork_and_exec{v,vp} の実装本体
+   
+ */
+static pid_t fork_and_exec( int (*exec)( const char* , char * const [] ) , const char* path ,  char* const argv[] );
+
+static pid_t fork_and_exec( int (* const exec)( const char* , char * const [] ) , const char* path ,  char* const argv[] )
+{
+  /*
+     TODO: 
+     * 関数の戻り値についての仕様をもうはっきりさせる必要がある。
+     * 関数自体が長すぎる 
+     * path が不適当なときにどうするかのチェック
+     * argv のチェック？ できる？
+     * 作成中に、戻り値が、int だったり pid_t だったりして、安定していない。
+     多分 fork の戻り値 pid_t をベースに考えるのが良いと思う。
+     * SIGCHLD の動作を考慮していない。 sigaction の動作を加えて考えること。
+     * 既に開いている ファイルディスクリプタの操作がまだ。
+     * 戻り値の pid_t の意味と、errno の状態についてまとめておくこと。
+   */
+  assert( exec );
+  if( exec ){ return -1; } // もうちょい 引数チェックを実行的に realpath 使うとか ?
+  
+  int pipefd[2] = { -1 , -1 };
+  if( -1 == pipe( pipefd  ) ){
+    perror( "pipe()" );
+  }
+  assert( 0 <= pipefd[READ_SIDE] );
+  assert( 0 <= pipefd[WRITE_SIDE] );
+  
+  /* 
+     書き込み側 は、O_CLOEXEC を付けておく
+     こうすることで、exec ファミリを呼び出したときに、
+     自動的に、書き込み側が閉じられるので、
+     親プロセス側では、read(2) で待っていると、
+     
+     exec が成功したら0byteのread に成功されて、
+     （つまり 書き込み側が exec に成功して、閉じたことを意味し）
+     exec が失敗した時には、そのerrno が親プロセス側で検知することができる。
+  */
+  int fcntl_result = -1;
+  VERIFY( 0 == ( fcntl_result = fcntl_set_close_exec( pipefd[ WRITE_SIDE ] ) ));
+  
+  pid_t result = -1; // この関数の戻り値
+
+  if( 0 != fcntl_result ){
+    return -1;
+  }
+  const pid_t pid = fork();
+  
+  switch( pid ){
+  case -1 :
+    {
+      abort(); // TODO:  abort() すると pipefd がリークする。
+      break;
+    }
+  case 0:
+    {
+      if( 0 != close( pipefd[ READ_SIDE ] )  ){
+        perror( "close()" );
+      }
+      if( -1 == exec( path , argv) ){
+        /* execl 失敗した時には、失敗した理由 errno を パイプに書き込んで終了する。*/
+        int b = errno;
+        VERIFY( sizeof( b ) == write( pipefd[WRITE_SIDE] , &b , sizeof( b ) ) );
+        VERIFY( 0 == close( pipefd[WRITE_SIDE] ) );
+        _exit(EXIT_SUCCESS);
+      }
+      break;
+    }
+  default:
+    {
+      if( 0 != close( pipefd[ WRITE_SIDE ] ) ){
+        perror( "close()" );
+      }
+      do{
+        // select で、
+        int b = 0;
+        const ssize_t read_v = read( pipefd[READ_SIDE] , &b  , sizeof(b) );
+        //printf( "read_v = %zd\n" , read_v );
+        if( read_v == 0 ){
+          errno = 0;
+        }else if( read_v == sizeof( b ) ){
+          /* exec に失敗した時には、関数内で プロセスの回収を行う */
+          {
+            int status = 0;
+            VERIFY( pid == waitpid( pid , &status , 0 ) );
+            VERIFY( EXIT_SUCCESS == status );
+          }
+          /* エラーメッセージの出力 */
+#if defined(  _GNU_SOURCE )
+#error use strerror_r(3) XSI-comiliant version. see strerror_r(3) manual.
+#endif/* defined(  _GNU_SOURCE ) */
+          char err_message[80];
+          if( 0 == strerror_r( b , err_message , sizeof( err_message ) / sizeof( err_message[0] ) ) ){
+            do{
+              {
+                if( -1 == write( STDERR_FILENO , err_message , strlen( err_message ) ) ){
+                  break;
+                }
+              }
+              {
+                const char space[] = " ";
+                if( -1 == write( STDERR_FILENO , space , strlen( space ) ) ){
+                  break;
+                }
+              }
+              {
+                if( -1 == write( STDERR_FILENO , path , strlen( path ) ) ){
+                  break;
+                }
+              }
+              {
+                const char terminate[] = "\n";
+                if( -1 == write( STDERR_FILENO , terminate , strlen( terminate ) ) ){
+                  break;
+                }
+              }
+            }while( (void)0,0 );
+            sync();
+          }
+          errno = b;
+        } /* end of if ( read_v == sizeof( b ) ) */
+      }while( (void)0, 0 );
+      VERIFY( 0 == close( pipefd[ READ_SIDE ] ) );
+      break; 
+    }
+  } // end of switch( pid )
+  return result;
+}
+
+
+
+/**
+
+ */
+int fork_and_execv( const char* path , char* const argv[] );
+
+int fork_and_execv( const char* path , char* const argv[] )
+{
+  return fork_and_exec( execv , path , argv );
+}
+
+/**
+
+ */
+int fork_and_execvp( const char* path , char* const argv[] );
+
+int fork_and_execvp( const char* path , char* const argv[] )
+{
+  return fork_and_exec( execvp , path , argv );
+}
 
 
 int main(int argc , char* argv[] )
@@ -175,7 +335,8 @@ int main(int argc , char* argv[] )
 
     if( 0 == fcntl_result ){
       const char path[] = "/bin/true";
-      switch( fork() ){
+      const pid_t pid = fork();
+      switch( pid ){
       case -1 :
         {
           abort();
@@ -206,6 +367,13 @@ int main(int argc , char* argv[] )
             const ssize_t read_v = read( pipefd[READ_SIDE] , &b  , sizeof(b) );
             //printf( "read_v = %zd\n" , read_v );
             if( read_v == sizeof( b ) ){
+              /* exec に失敗した時には、関数内で プロセスの回収を行う */
+              {
+                int status = 0;
+                VERIFY( pid == waitpid( pid , &status , 0 ) );
+                VERIFY( EXIT_SUCCESS == status );
+              }
+              /* エラーメッセージの出力 */
 #if defined(  _GNU_SOURCE )
 #error use strerror_r(3) XSI-comiliant version. see strerror_r(3) manual.
 #endif/* defined(  _GNU_SOURCE ) */
